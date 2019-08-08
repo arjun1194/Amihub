5159248727f3d34ec0b59729b2e83acd03f115c2
Avinash Kumar <aviras@live.com>
2019-08-09 02:33:35 +0530

    Captcha done

--- lib/captcha_page.dart
+++ lib/captcha_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'dart:convert';
 import 'package:amihub/Theme/theme.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+import 'ViewModels/captcha_model.dart';
 import 'ViewModels/login_model.dart';
 
 class CaptchaPageNew extends StatefulWidget {
@@ -15,6 +15,7 @@ class _CaptchaPageNewState extends State<CaptchaPageNew> {
   WebViewController _controller;
 
   bool isRendered = false;
+  var _stackIndex;
 
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
@@ -49,6 +50,12 @@ class _CaptchaPageNewState extends State<CaptchaPageNew> {
           setState(() {
             isRendered = true;
           });
+          Timer(Duration(milliseconds: 100), () {
+            setState(() {
+              _stackIndex = 0;
+            });
+          });
+
           _clearWebsite(_controller, context);
         });
   }
@@ -56,6 +63,7 @@ class _CaptchaPageNewState extends State<CaptchaPageNew> {
   @override
   void initState() {
     super.initState();
+    _stackIndex = 1;
   }
 
   getCaptchaResponse(
@@ -67,44 +75,54 @@ class _CaptchaPageNewState extends State<CaptchaPageNew> {
   Widget build(BuildContext context) {
     final LoginModel args = ModalRoute.of(context).settings.arguments;
     return Scaffold(
-      key: _scaffoldKey,
-        body: Stack(
-      children: <Widget>[
-        Align(
-          alignment: Alignment.center,
-          child: _webView(),
-        ),
-        isRendered
-            ? Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: EdgeInsets.only(bottom: 100),
-                  child: RaisedButton(
-                    child: Text("LOGIN"),
-                    color: Colors.blueGrey[400],
-                    splashColor: Colors.blueGrey[200],
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0)),
-                    textColor: Colors.white,
-                    highlightElevation: 5.0,
-                    elevation: 7.0,
-                    onPressed: () async {
-                      String a = await getCaptchaResponse(_controller, context);
-                      if (a.length > 10) {
-                        print(a.substring(1, a.length - 1));
-                        print('username = ${args.username}');
-                        print('password = ${args.password}');
-                      } else {
-                        _scaffoldKey.currentState.showSnackBar(SnackBar(
-                          content: Text("Please slove the recaptcha"),
-                        ));
-                      }
-                    },
-                  ),
+        key: _scaffoldKey,
+        body: IndexedStack(
+          index: _stackIndex,
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.center,
+                  child: _webView(),
                 ),
-              )
-            : Container()
-      ],
-    ));
+                isRendered
+                    ? Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: EdgeInsets.only(bottom: 100),
+                          child: RaisedButton(
+                            child: Text("LOGIN"),
+                            color: Colors.blueGrey[400],
+                            splashColor: Colors.blueGrey[200],
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0)),
+                            textColor: Colors.white,
+                            highlightElevation: 5.0,
+                            elevation: 7.0,
+                            onPressed: () async {
+                              String a = await getCaptchaResponse(
+                                  _controller, context);
+                              if (a.length > 10) {
+                                String captcha = a.substring(1, a.length - 1);
+                                CaptchaModel captchaModel = CaptchaModel(
+                                    args.username, args.password, captcha);
+                                Navigator.pushNamedAndRemoveUntil(context,
+                                    '/load', (Route<dynamic> route) => false,
+                                    arguments: captchaModel);
+                              } else {
+                                _scaffoldKey.currentState.showSnackBar(SnackBar(
+                                  content: Text("Please slove the recaptcha"),
+                                ));
+                              }
+                            },
+                          ),
+                        ),
+                      )
+                    : Container()
+              ],
+            ),
+            Center(child: CircularProgressIndicator())
+          ],
+        ));
   }
 }
