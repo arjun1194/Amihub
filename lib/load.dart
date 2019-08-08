41c5748528b4b6885d92c025992ed2d684aec95c
arjun1194 <arjunbhd194@gmail.com>
2019-08-09 00:53:40 +0530

    Removed Dio , Now using http.dart

--- lib/load.dart
+++ lib/load.dart
@@ -2,42 +2,30 @@ import 'dart:convert';
 import 'package:amihub/Theme/theme.dart';
 import 'package:amihub/ViewModels/captcha_model.dart';
 import 'package:amihub/home.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class LoadApi extends StatefulWidget {
   @override
   _LoadApiState createState() => _LoadApiState();
 }
 
-Future<String> isLoginSuccessful(username, password, gCaptcha, context) async {
-  print("username $username password $password Captcha $gCaptcha");
 
-  Response response;
-  Dio dio = new Dio();
-  String url = amihubUrl + "/login";
-  response = await dio.get(url, queryParameters: {
-    "username": username,
-    "password": password,
-    "captchaResponse": gCaptcha
-  });
-  print(response.data.toString());
-
-  if (response.statusCode == 201) {
-    print("Auth------------------>" + response.headers.value('Authorization'));
-    Navigator.of(context).pushNamedAndRemoveUntil(
-      '/home',
-      (Route<dynamic> route) => false,
-    );
-  }
-  if (response.statusCode == 401) {
-    return "Either the username or password is incorrect!";
-  }
-
-  return response.data.toString();
-}
 
 class _LoadApiState extends State<LoadApi> {
+
+  isLoginSuccessful(String username,String password,String gcaptcha,BuildContext context){
+    loginWithUsername(username, password, gcaptcha).then((Response resp){
+        if(resp.statusCode==201){
+          //login was successful so get JWT as a response body and use it to fetch other API's
+          //save the JWT in sharedPreferences
+          //Navigator.pushNamedRemoveUntil(/home);
+        }
+    });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     final CaptchaModel args = ModalRoute.of(context).settings.arguments;
@@ -81,8 +69,19 @@ class _LoadApiState extends State<LoadApi> {
     );
   }
 
+
+
   @override
   void didUpdateWidget(LoadApi oldWidget) {
     print("somthing changed!!!!!!!!!!!!!!!");
   }
+
+  Future<Response> loginWithUsername(String username,String password,String gcaptcha) {
+    Client client = Client();
+    String url = amihubUrl+"/login?username=$username&password=$password&captchaResponse=$gcaptcha";
+    return client.get(url);
+
+  }
+
+
 }
