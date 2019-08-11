38162dfdd1a2af809db6a4ade46e97cfe25473ee
arjun1194 <arjunbhd194@gmail.com>
2019-08-11 10:56:46 +0530

    Added left files

---
+++ lib/ViewModels/today_class_model.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TodayClass {
+  Color attendanceColor;
+  String  courseCode;
+  String  courseTitle;
+  String facultyName;
+  String roomNo;
+  String startTime;
+  String endTime;
+
+TodayClass( this.attendanceColor, this.courseCode, this.courseTitle,
+  this.facultyName, this.roomNo, this.startTime, this.endTime);
+}
+
+
