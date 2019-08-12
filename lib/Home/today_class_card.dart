1115fe732bbca701bb690efda5e98103576f5e7c
arjun1194 <arjunbhd194@gmail.com>
2019-08-13 04:04:40 +0530

    Housekeeping

--- lib/Home/today_class_card.dart
+++ lib/Home/today_class_card.dart
@@ -9,42 +9,40 @@ class TodayClassCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 8,bottom: 8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(todayClass.courseCode,style: TextStyle(fontSize: 20,fontFamily: "Raleway",color: Colors.white),),
-                    Text(todayClass.startTime,style: TextStyle(fontSize: 20,fontFamily: "Raleway",color: Colors.white),),
+    var width = MediaQuery.of(context).size.width;
+    return Container(
+      width: width,
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 8,bottom: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(todayClass.courseCode,style: TextStyle(fontSize: 20,color: Colors.white),),
+                  Text(todayClass.startTime,style: TextStyle(fontSize: 20,color: Colors.white),),
 
-                  ],),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Text(todayClass.courseTitle,style: TextStyle(fontSize: 20,fontFamily: "Raleway",color: Colors.white),),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Text(todayClass.facultyName,style: TextStyle(fontSize: 20,fontFamily: "Raleway",color: Colors.white),),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Text(todayClass.roomNo,style: TextStyle(fontSize: 20,fontFamily: "Raleway",color: Colors.white),),
-              ),
+                ],),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text(todayClass.courseTitle,overflow: TextOverflow.ellipsis, style: TextStyle(fontSize: 20,color: Colors.white),),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text(todayClass.facultyName,style: TextStyle(fontSize: 20,color: Colors.white),),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text(todayClass.roomNo,style: TextStyle(fontSize: 20,color: Colors.white),),
+            ),
 
-            ],),
-        ),
+          ],),
+        color:color,
+        shape: RoundedRectangleBorder(borderRadius:BorderRadius.all(Radius.circular(10))),
       ),
-      color:color,
-      shape: RoundedRectangleBorder(borderRadius:BorderRadius.all(Radius.circular(10))),
     );
   }
 }
