0c5fdffb2a682c569bb1348552c7c524aca93ae4
arjun1194 <arjunbhd194@gmail.com>
2019-08-13 04:09:03 +0530

    [Added] splash screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    SharedPreferences.getInstance().then((sharedPreferences){
+      if(sharedPreferences.containsKey('Authorization') && checkAuthValid()){
+        print('<---JWT is detected--->');
+
+          print('<---JWt is valid : logging you in --->');
+          Navigator.pushNamedAndRemoveUntil(context, '/home', (Route<dynamic> route) => false);
+      }
+      else{
+        print("invalid JWT or JWT expired");
+        if(sharedPreferences.containsKey('Authorization'))sharedPreferences.remove('Authorization');
+        Navigator.pushNamedAndRemoveUntil(context,'/login', (Route<dynamic> route) => false);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body:Center(child: Text('InsertLogo here'),),);
+  }
+
+  bool checkAuthValid() {return true;}
+
+
+}
