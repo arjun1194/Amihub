95ae74883aa211bd91870ab6db4fed243fd5e5f5
arjun1194 <arjunbhd194@gmail.com>
2019-08-13 04:03:16 +0530

    [Fixed] today class cards horizontal

--- lib/Home/home_future_builder.dart
+++ lib/Home/home_future_builder.dart
@@ -1,7 +1,6 @@
 import 'package:amihub/Components/random_color.dart';
 import 'package:amihub/Home/today_class_card.dart';
 import 'package:amihub/Repository/amizone_repository.dart';
-import 'package:amihub/Theme/theme.dart';
 import 'package:amihub/ViewModels/today_class_model.dart';
 import 'package:flutter/material.dart';
 
@@ -20,32 +19,30 @@ class _TodayClassBuilderState extends State<TodayClassBuilder> {
       builder:
           (BuildContext context, AsyncSnapshot<List<TodayClass>> snapshot) {
         switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
-            return Center(
-                child: Text(
-              'Awaiting result...',
-              style: headingStyle,
-            ));
+          case ConnectionState.waiting:return Center(child: CircularProgressIndicator());
           case ConnectionState.done:
-            if (snapshot.hasError)
-              return Center(child: Text('Error: ${snapshot.error}'));
-            return Column(
+            if (snapshot.hasError) return Center(child: Text('Error: ${snapshot.error}'));
+            return Row(
               children: List.generate(
-                  snapshot.data==null?1:snapshot.data.length,
-                  (index) {
-                    if(snapshot.data==null){return TodayClassCard(TodayClass(Color(0xff),"","No Classes Today","","","",""),createRandomColor());}
-                    return TodayClassCard(
-                      snapshot.data.elementAt(index), createRandomColor());}),
+                  snapshot.data == null ? 1 : snapshot.data.length, (index) {
+                if (snapshot.data == null) {
+                  return TodayClassCard(
+                      TodayClass(
+                          Color(0xff), "", "No Classes Today", "", "", "", ""),
+                      createRandomColor());
+                }
+                return TodayClassCard(
+                    snapshot.data.elementAt(index), createRandomColor());
+              }),
             );
-          case ConnectionState.none:
-            // TODO: Handle this case.
-            break;
-          case ConnectionState.active:
-            // TODO: Handle this case.
-            break;
+          case ConnectionState.none:break;
+          case ConnectionState.active:break;
         }
         return Text("End"); // unreachable
       },
     );
   }
 }
+
+
+
