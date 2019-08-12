f21be80c5869291bae188f27e1c1e1939662af8e
arjun1194 <arjunbhd194@gmail.com>
2019-08-13 04:14:10 +0530

    Loads Splash Screen first

--- lib/Routes/routes.dart
+++ lib/Routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:amihub/captcha.dart';
+import 'package:amihub/splash_screen.dart';
 import 'package:amihub/forgot_password.dart';
 import 'package:amihub/home.dart';
 import 'package:amihub/load.dart';
@@ -9,10 +10,10 @@ import 'package:flutter/material.dart';
 import '../captcha_page.dart';
 
 Map<String, WidgetBuilder> routes = {
-  '/': (BuildContext context) => HomePage(),
-  '/home': (BuildContext context) => MyApp(),
+  '/': (BuildContext context) => SplashScreen(),
+  '/home': (BuildContext context) => HomePage(),
   '/captcha': (BuildContext context) => CaptchaPageNew(),
   '/forgotPassword': (BuildContext context) => ForgotPassword(),
   '/load': (BuildContext context) => LoadApi(),
-  '/homepage': (BuildContext context) => HomePage(),
+  '/login': (BuildContext context) => LoginPage(),
 };
