88861a5065d91bf827f20a74ae79e998bb5297c7
arjun1194 <arjunbhd194@gmail.com>
2019-08-13 04:11:52 +0530

    added BNB widgets

---
+++ lib/Home/Body/home_body.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../home_future_builder.dart';
+
+
+class HomeBody extends StatefulWidget {
+  @override
+  _HomeBodyState createState() => _HomeBodyState();
+}
+
+class _HomeBodyState extends State<HomeBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("Today's Classes",style:TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+        Container(height: 200,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[TodayClassBuilder(),],
+          ),
+        ),
+      ],
+    );
+  }
+}
